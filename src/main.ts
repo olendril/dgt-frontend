b96a70222be1f535a0391944c4541370776b1b25
Mohan Grewis <mohan.grewis@gmail.com>
2024-11-22 16:38:36 +0100

    Sync2

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,12 @@ import App from './App.vue'
 import './style.css'
 import router from './router'
 import { createPinia } from 'pinia'
-import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
 
 
 const app = createApp(App)
 
 app.use(router)
 
-app.use(PrimeVue, {
-    theme: {
-        preset: Aura
-    }
-});
 app.use(createPinia())
 
 app.mount('#app')
