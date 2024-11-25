147c9f287b0101c28eef3e16598d811a9ea6607d
Mamyliss <namyliss@gmail.com>
2024-11-25 22:26:25 +0100

    footer

--- src/App.vue
+++ src/App.vue
@@ -29,8 +29,13 @@ function toggleDarkMode() {
 
     <RouterView />
 
-    <footer class="flex space-x-4 justify-around max-h-28">
-      <p>coffer</p>
+    <footer class="flex space-x-4 justify-between max-h-28 p-2">
+      <a href='https://ko-fi.com/O5O016KSBS' target='_blank'>
+        <img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi2.png?v=6' border='0' alt='Buy Me a Coffee at ko-fi.com' />
+      </a>
+      <p class="text-center">Dofus est un MMORPG édité par Ankama. "dofuccès" est un site non-officiel sans aucun lien avec Ankama.
+        Certaines illustrations sont la propriété d'Ankama Studio et de Dofus - Tous droits réservés
+        Copyright © 2024 Dofus pour les Noobs - Merci de ne pas copier le contenu de ce site sans autorisation.</p>
       <Button label="Toggle Dark Mode" @click="toggleDarkMode()" />
     </footer>
 
