ca96d61c3695d8d450dc4281e97fcd2bd4187039
Mohan Grewis <mohan.grewis@gmail.com>
2024-11-25 15:23:14 +0100

    Add Theme

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,38 @@ import router from './router'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
 import Lara from '@primevue/themes/lara';
+import { definePreset } from '@primevue/themes';
 
 const app = createApp(App)
 
 app.use(router)
 
 app.use(createPinia())
+const MyPreset = definePreset(Lara, {
+    semantic: {
+        primary: {
+            50: '{amber.50}',
+            100: '{amber.100}',
+            200: '{amber.200}',
+            300: '{amber.300}',
+            400: '{amber.400}',
+            500: '{amber.500}',
+            600: '{amber.600}',
+            700: '{amber.700}',
+            800: '{amber.800}',
+            900: '{amber.900}',
+            950: '{amber.950}'
+        }
+    }
+});
+
 app.use(PrimeVue, {
     theme: {
-        preset: Lara,
+        preset: MyPreset,
         options: {
             prefix: 'p',
-            darkModeSelector: 'system',
-            cssLayer: false
+            darkModeSelector: false,
+            cssLayer: false,
         }
     }
 });
